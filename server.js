5774c33f73b3cd08ca012a4f6a8e553c022ff206
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2016-09-10 16:30:03 -0500

    index modifies with grupo acera web page (without css,js)

--- server.js
+++ server.js
@@ -6,13 +6,28 @@ var express = require('express'),
 //send our index.html to the user for the home page
 
 app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile(path.join(__dirname+'/src/webpage.html'));
 });
 
 //create routes for the admin section
 
 //get an instance of the router
 var adminRouter = express.Router();
+
+//route middleware that will happen on every
+ 
+ adminRouter.use(function(req,res,next){
+ 	//log each request to the console
+ 	console.log(req.method, req.url);
+
+ 	//continue doing some stuff
+ 	next();
+ })
+
+
+
+
+
 //admin main page. the dashboard  (http://localhost:1337/admin)
 adminRouter.get('/', function(req,res){
 	res.send('i am the dashboard');
