1791aa95aabf346204ddb170bc4e68f2a70d1048
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2016-09-10 16:16:38 -0500

    express.Router() included in app, root app called 'route'

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+//create routes for the admin section
+
+//get an instance of the router
+var adminRouter = express.Router();
+//admin main page. the dashboard  (http://localhost:1337/admin)
+adminRouter.get('/', function(req,res){
+	res.send('i am the dashboard');
+});
+
+//users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req,res){
+	res.send('i show all the users!');
+});
+//posts page (http://localhost:1337/admin/post)
+
+adminRouter.get('/posts', function(req,res){
+	res.send('i show all the posts!');
+});
+
+//apply the routes to our aplication
+app.use('/app', adminRouter);
+
+
+
 //start the server
 
 app.listen(1337);
